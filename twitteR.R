60c6798eb94e88e07eeeb2bdf81ef5d6c5c7ec0e
ciaranRoche <ciaranroche@hotmail.co.uk>
2017-04-03 14:37:32 +0100

    function added to compute the raw sentiment

--- twitteR.R
+++ twitteR.R
@@ -109,4 +109,31 @@ head(opinion.lexicon.pos)
 
 
 
+#computes the raw sentiment based on the simple matching algorithm:
+getSentimentScore = function(sentences, words.positive, words.negative, .progress='none')
+{
+   require(plyr)
+   require(stringr)
+   scores = laply(sentences,
+   function(sentence, words.positive, words.negative) {
+  # Let first remove the Digit, Punctuation character and Control characters:
+   sentence = gsub('[[:cntrl:]]', '', gsub('[[:punct:]]', '', gsub('\\d+', '', sentence)))
+  # Then lets convert all to lower sentence case:
+   sentence = tolower(sentence)
+  # Now lets split each sentence by the space delimiter
+   words = unlist(str_split(sentence, '\\s+'))
+  # Get the boolean match of each words with the positive & negative
+   opinion-lexicon
+   pos.matches = !is.na(match(words, words.positive))
+   neg.matches = !is.na(match(words, words.negative))
+  # Now get the score as total positive sentiment minus the total negatives
+   score = sum(pos.matches) - sum(neg.matches)
+   return(score)
+}, words.positive, words.negative, .progress=.progress )
+  # Return a data frame with respective sentence and the score
+   return(data.frame(text=sentences, score=scores))
+}
+
+
+
 
